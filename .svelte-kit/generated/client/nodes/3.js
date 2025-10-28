113bbd1e158f59a5ddcdd82185a43362d1235275
onebeastchris <github@onechris.mozmail.com>
2025-10-28 16:21:33 +0100

    Yeet ignored

--- .svelte-kit/generated/client/nodes/3.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/adopted_projects/+page.svelte";
--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/our_team/+page.svelte";
